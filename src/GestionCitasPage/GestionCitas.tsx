3783b17424a6aec997a9155b88893bf62a5f76ec
AlejandroAHF <aleherfernandez@gmail.com>
2024-10-12 22:36:43 -0600

    eliminacion de importes innecesarios

--- src/GestionCitasPage/GestionCitas.tsx
+++ src/GestionCitasPage/GestionCitas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from '../Share/Componentes/Navbar/Navbar';
 import CitasMedicasServicios from '../Services/CitasMedicasServicios';
 
